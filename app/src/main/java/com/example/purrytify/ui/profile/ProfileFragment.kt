0e417ec2be47181fa0aefb4387c7deda21ec7afb
Yusufarsan <sandiyusufardian@gmail.com>
2025-04-12 16:09:46 +0700

    chore: optimize import

--- app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.purrytify.R
