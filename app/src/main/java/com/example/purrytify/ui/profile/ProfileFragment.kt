a891999b9c8ce53d2c86c3ff9013c198f3ccc586
Yusufarsan <sandiyusufardian@gmail.com>
2025-04-12 18:31:52 +0700

    feat: change ProfileScreen() to get data from global

--- app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
@@ -8,20 +8,29 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.purrytify.ui.shared.SharedViewModel
 import com.example.purrytify.R
 import com.example.purrytify.databinding.FragmentProfileBinding
 import com.example.purrytify.ui.theme.PurrytifyTheme
 import com.example.purrytify.util.NetworkUtil
+import kotlinx.coroutines.flow.collectLatest
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-    private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var profileViewModel: ProfileViewModel // TODO: Potentially unused
 
-    override fun onCreateView(
+    private val sharedViewModel: SharedViewModel by lazy {
+        ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+    }
+
+    override fun onCreateView(  // TODO: Potentially unused
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,9 +54,11 @@ class ProfileFragment : Fragment() {
                         Log.d("ProfileFragment", "No internet connection")
                         NetworkUtil.NoInternetScreen()
                     } else {
-                        // Fetch user profile data if network is available
-                        profileViewModel.fetchUserProfile(requireContext())
-                        ProfileScreen(viewModel = profileViewModel)
+                        // Get global user profile from SharedViewModel
+                        val userProfile = sharedViewModel.globalUserProfile.collectAsState().value
+                        userProfile?.let {
+                            ProfileScreen(userProfile = it)
+                        }
                     }
                 }
             }
