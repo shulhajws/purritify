a0053b54b67ed9a2f95110ec1f760fa016a4c171
Yusufarsan <sandiyusufardian@gmail.com>
2025-04-12 16:08:55 +0700

    feat: added handle internet issue in Profile Page

--- app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.purrytify.R
 import com.example.purrytify.databinding.FragmentProfileBinding
 import com.example.purrytify.ui.theme.PurrytifyTheme
+import com.example.purrytify.util.NetworkUtil
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -33,16 +40,21 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Fetch user profile data
-        profileViewModel.fetchUserProfile(requireContext())
-
-        binding.profileComposeView.setContent {
-            PurrytifyTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ProfileScreen(viewModel = profileViewModel)
+        if (!NetworkUtil.isNetworkAvailable(requireContext())) {
+            binding.profileComposeView.setContent {
+                NetworkUtil.NoInternetScreen()
+            }
+        } else {
+            // Fetch user profile data if network is available
+            profileViewModel.fetchUserProfile(requireContext())
+            binding.profileComposeView.setContent {
+                PurrytifyTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        ProfileScreen(viewModel = profileViewModel)
+                    }
                 }
             }
         }
