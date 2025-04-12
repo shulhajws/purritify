0dbfb46f322f9afce160676ea9d2b9e35e45935f
Yusufarsan <sandiyusufardian@gmail.com>
2025-04-12 16:08:10 +0700

    feat: added NoInternetScreen() method in NetworkUtil

--- app/src/main/java/com/example/purrytify/util/NetworkUtil.kt
+++ app/src/main/java/com/example/purrytify/util/NetworkUtil.kt
@@ -5,6 +5,42 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+// Code below are Example of call NetworkUtil to handle internet issue in ProfileFragment.kt
+// 1. Check if the network is available in onViewCreated
+// 2. If not available, show NoInternetScreen
+// 3. Add internet issue handling also inside fetch data method
+
+//override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//    super.onViewCreated(view, savedInstanceState)
+//
+//    if (!NetworkUtil.isNetworkAvailable(requireContext())) {
+//        binding.profileComposeView.setContent {
+//            NetworkUtil.NoInternetScreen()
+//        }
+//    } else {
+//        profileViewModel.fetchUserProfile(requireContext())
+//        binding.profileComposeView.setContent {
+//            PurrytifyTheme {
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    ProfileScreen(viewModel = profileViewModel)
+//                }
+//            }
+//        }
+//    }
+//}
 
 object NetworkUtil {
     private var isConnected = true
@@ -34,5 +70,21 @@ object NetworkUtil {
         val activeNetwork = connectivityManager.activeNetwork ?: return false
         val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
         return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+    @Composable
+    fun NoInternetScreen() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "No internet connection. Please check your connection.",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Black
+            )
+        }
     }
 }
