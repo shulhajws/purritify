54320067cec1af7a5c023b48c0c4280ce0bf9653
Yusufarsan <sandiyusufardian@gmail.com>
2025-04-12 23:05:19 +0700

    chore: optimize import

--- app/src/main/java/com/example/purrytify/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/purrytify/ui/login/LoginActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.purrytify.MainActivity
-import com.example.purrytify.ui.home.HomeScreen
 import com.example.purrytify.ui.theme.PurrytifyTheme
 
 class LoginActivity : ComponentActivity() {
