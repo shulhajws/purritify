e66dcbae156291c6767bb3a120afd03355c5711d
Yusufarsan <sandiyusufardian@gmail.com>
2025-04-12 19:05:21 +0700

    feat: added log out button to profile no network page

--- app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/purrytify/ui/profile/ProfileFragment.kt
@@ -1,24 +1,39 @@
 package com.example.purrytify.ui.profile
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.purrytify.ui.shared.SharedViewModel
 import com.example.purrytify.R
 import com.example.purrytify.databinding.FragmentProfileBinding
+import com.example.purrytify.ui.login.LoginActivity
 import com.example.purrytify.ui.theme.PurrytifyTheme
+import com.example.purrytify.ui.theme.White
 import com.example.purrytify.util.NetworkUtil
+import com.example.purrytify.util.TokenManager
 import kotlinx.coroutines.flow.collectLatest
 
 class ProfileFragment : Fragment() {
@@ -53,6 +68,38 @@ class ProfileFragment : Fragment() {
                     if (!NetworkUtil.isNetworkAvailable(requireContext())) {
                         Log.d("ProfileFragment", "No internet connection")
                         NetworkUtil.NoInternetScreen()
+                        val context = LocalContext.current
+
+                        // Logout button
+                        Button(
+                            onClick = {
+                                // Clear the token
+                                TokenManager.clearToken(context)
+
+                                // Navigate to LoginActivity
+                                val intent = Intent(context, LoginActivity::class.java)
+                                context.startActivity(intent)
+
+                                // Toast message
+                                Toast.makeText(context, "Logged out successfully", Toast.LENGTH_SHORT).show()
+
+                                // Finish current activity "if needed"
+                                (context as? Activity)?.finish()
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Red
+                            ),
+                            shape = RoundedCornerShape(percent = 50),
+                            modifier = Modifier
+                                .fillMaxWidth(0.5f)
+                                .height(48.dp)
+                        ) {
+                            Text(
+                                text = "Logout",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = White
+                            )
+                        }
                     } else {
                         // Get global user profile from SharedViewModel
                         val userProfile = sharedViewModel.globalUserProfile.collectAsState().value
